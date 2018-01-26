3680bb022008f0511deb8c5221ebbb3e7144cbfa
Rian Fowler <rian.fowler@adhocteam.us>
2018-01-26 15:32:49 -0600

    update babel export to target all jsx files

--- src/js/export-components.js
+++ src/js/export-components.js
@@ -1,19 +1,67 @@
 const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const recursiveReadDir = require('recursive-readdir');
 const babel = require("babel-core");
 
-const reactComponentFiles = [
-  'src/components/errorable_checkbox/ErrorableCheckbox.jsx',
-  'src/components/modal/Modal.jsx',
-  'src/components/search_menu/SearchMenu.jsx',
-  'src/components/segmented_progress_bar/SegmentedProgressBar.jsx',
-  'src/components/tooltip/Tooltip.jsx'
-];
 
-reactComponentFiles.forEach((fileName) => {
-  babel.transformFile(fileName, (err, result) => {
-    const componentName = fileName.slice(fileName.lastIndexOf('/') + 1, fileName.indexOf('.'));
-    const newFileName = `dist/js/components/${componentName}.js`;
-
-    fs.writeFile(newFileName, result.code);
+function maybeMakeDirectories(directories = []) {
+  directories.forEach((directory) => {
+    if(!fs.existsSync(directory)) {
+      mkdirp.sync(directory);
+      console.log(`${directory} directory created`);
+    }
   });
-});
+}
+
+function exportFiles(sourceFiles = [], destinationDirectory = '/dist') {
+  sourceFiles.forEach((fileName) => {
+    babel.transformFile(fileName, (err, result) => {
+      const componentName = fileName.slice(fileName.lastIndexOf('/') + 1, fileName.indexOf('.'));
+      const newFileName = `${destinationDirectory}/${componentName}.js`;
+
+      fs.writeFile(newFileName, result.code);
+    });
+  });
+}
+
+function ignoredFilesPredicate(file, stats) {
+  return !stats.isDirectory() && // don't ignore directories
+    (file.includes('.spec.jsx') || // ignore test files
+    !file.includes('.jsx')) // don't ignore .jsx files
+}
+
+function getComponentsPaths(componentsDirectory) {
+  return recursiveReadDir(componentsDirectory, [ignoredFilesPredicate]);
+}
+
+function transformJSFiles(filePaths = []) {
+  return Promise.resolve(filePaths.map((filePath) =>
+    ({
+      name: path.basename(filePath).slice(0, -1),
+      code: babel.transformFileSync(filePath).code
+    })
+  ));
+}
+
+function exportFiles(destinationDirectory, transformedCodeObjects) {
+  transformedCodeObjects.forEach((file) =>
+    fs.writeFileSync(`${destinationDirectory}/${file.name}`, file.code)
+  );
+
+  console.log(`\x1b[36m${transformedCodeObjects.length} files written`);
+}
+
+function main() {
+  const appRootPath = path.resolve();
+  const distComponentsPath = `${appRootPath}/dist/js/components`;
+
+  maybeMakeDirectories([distComponentsPath]);
+
+  getComponentsPaths(`${appRootPath}/src/components`)
+    .then(transformJSFiles)
+    .then((transformedCodeObjects) =>
+      exportFiles(distComponentsPath, transformedCodeObjects));
+}
+
+main();
